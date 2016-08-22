483f11140941df738e543663a9cc06bedbf885ad
Túlio Filho <tulio@analiselogica.com.br>
2016-08-22 11:25:16 -0300

    Removendo referencias

--- Caelum.Leilao.Tests/TesteDoAvaliador.cs
+++ Caelum.Leilao.Tests/TesteDoAvaliador.cs
@@ -1,8 +1,6 @@
-﻿using System;
+﻿using NUnit.Framework;
 
-using NUnit.Framework;
-
-namespace Caelum.Leilao.Teste
+namespace Caelum.Leilao
 {
     [TestFixture]
     public class TesteDoAvaliador
--- Caelum.Leilao/UsoDoAvaliador.cs
+++ Caelum.Leilao/UsoDoAvaliador.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Caelum.Leilao
 {
@@ -10,27 +6,21 @@ namespace Caelum.Leilao
     {
         static void Main(String[] args)
         {
-            // 1a parte: Cenario
             Usuario joao = new Usuario("João");
             Usuario jose = new Usuario("José");
             Usuario maria = new Usuario("Mária");
 
             Leilao leilao = new Leilao("Playstation 3 Novo");
 
-            leilao.Propoe(new Lance(maria, 250.0));
             leilao.Propoe(new Lance(joao, 300.0));
             leilao.Propoe(new Lance(jose, 400.0));
+            leilao.Propoe(new Lance(maria, 250.0));
 
-            // 2a parte: Acao
             Avaliador leiloeiro = new Avaliador();
             leiloeiro.Avalia(leilao);
 
-            // 3a parte: validacao
-            double maiorEsperado = 400;
-            double menorEsperado = 250;
-
-            Console.WriteLine(maiorEsperado == leiloeiro.MaiorLance);
-            Console.WriteLine(menorEsperado == leiloeiro.MenorLance);
+            Console.WriteLine(leiloeiro.MaiorLance);
+            Console.WriteLine(leiloeiro.MenorLance);
 
             Console.ReadKey();
         }
