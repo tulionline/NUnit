c39b14b55d308e80e39c8e44675f400c09f15246
Túlio Filho <tulio@analiselogica.com.br>
2016-08-22 10:58:16 -0300

    Usando o avaliador

--- Caelum.Leilao/UsoDoAvaliador.cs
+++ Caelum.Leilao/UsoDoAvaliador.cs
@@ -6,31 +6,25 @@ using System.Threading.Tasks;
 
 namespace Caelum.Leilao
 {
-    public class TesteDoAvaliador
+    public class UsoDoAvaliador
     {
         static void Main(String[] args)
         {
-            // 1a parte: Cenario
             Usuario joao = new Usuario("João");
             Usuario jose = new Usuario("José");
             Usuario maria = new Usuario("Mária");
 
             Leilao leilao = new Leilao("Playstation 3 Novo");
 
-            leilao.Propoe(new Lance(maria, 250.0));
             leilao.Propoe(new Lance(joao, 300.0));
             leilao.Propoe(new Lance(jose, 400.0));
+            leilao.Propoe(new Lance(maria, 250.0));
 
-            // 2a parte: Acao
             Avaliador leiloeiro = new Avaliador();
             leiloeiro.Avalia(leilao);
 
-            // 3a parte: validacao
-            double maiorEsperado = 400;
-            double menorEsperado = 250;
-
-            Console.WriteLine(maiorEsperado == leiloeiro.MaiorLance);
-            Console.WriteLine(menorEsperado == leiloeiro.MenorLance);
+            Console.WriteLine(leiloeiro.MaiorLance);
+            Console.WriteLine(leiloeiro.MenorLance);
 
             Console.ReadKey();
         }
